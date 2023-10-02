8e515a587c2827b74c9532d38c50bbdcb9691412
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2023-10-02 23:02:47 +0900

    Remove comments

--- pytablewriter_altcol_theme/plugin.py
+++ pytablewriter_altcol_theme/plugin.py
@@ -16,43 +16,9 @@ def _calc_other_ground_color(color: Color) -> Color:
     return Color("white")
 
 
-"""
-def col_separator_style_filter(
-    lcell: Optional[Cell], rcell: Optional[Cell], **kwargs: Any
-) -> Optional[Style]:
-    fg_color = None
-    bg_color = None
-
-    cell = lcell if lcell else rcell
-    if cell is None:
-        return None
-
-    #if cell.is_header_row():
-    #    return None
-
-    row = cell.row
-    col = cell.col
-    color = Color(kwargs.get("color", DEFAULT_COLOR))
-
-    if col % 2 == 0:
-        fg_color = _calc_other_ground_color(color)
-        bg_color = color
-    else:
-        fg_color = color
-
-    if fg_color or bg_color:
-        return Style(color=fg_color, bg_color=bg_color)
-
-    return None
-# """
-
-
 def style_filter(cell: Cell, **kwargs: Any) -> Optional[Style]:
     fg_color: Optional[Color] = None
     bg_color: Optional[Color] = None
-
-    # if cell.is_header_row():
-    #    return None
 
     color = Color(kwargs.get("color", DEFAULT_COLOR))
 
